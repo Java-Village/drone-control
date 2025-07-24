aba4f0ba19598070a7ef71b1bd0b499ecbc7374a
Aydin O'Leary <aydinoleary@gmail.com>
2025-07-24 13:02:21 -0700

    add image relay w/ bounding boxes

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import datetime as dt
 import os
 import socket
 import requests
+import math
 
 import drone_pb2
 
@@ -15,7 +16,7 @@ WIDTH = 640
 HEIGHT = 480
 CHUNK_SIZE = 4096
 
-BACKEND_URL = "10.42.7.5:4181/api/panels" # TODO: change to exposed endpoint on jetnet
+BACKEND_URL = "http://10.42.7.111:4181/api/panels"
 
 WEIGHTS_PATH = "./weights/yolo_nano_solar_panel.pt"
 DEST_DIR = "/home/aicps/tcp_server/images"
@@ -42,40 +43,34 @@ class ImageServerProtocol:
         print(image_array.shape)
 
         timestamp = dt.datetime.now().strftime("%Y%m%d%H%M%S_%f")
-        image_path = os.path.join(DEST_DIR, f"{timestamp}.jpg")
+        image_path = os.path.join(DEST_DIR, f"{timestamp}.png")
         cv.imwrite(image_path, image_array)
 
-        results = self.model.predict(image_array, imgsz=(HEIGHT, WIDTH))
+        print("runnning prediction model")
+        results = self.model.predict(image_array)
         boxes = results[0].boxes
         update = drone_pb2.PictureUpdate()
+        timestamp = dt.datetime.now().strftime("%Y%m%d%H%M%S_%f")
+        image_path = os.path.join(DEST_DIR, f"{timestamp}_pred.png")
+        results[0].show()
+        results[0].save(filename=image_path)
 
         # add metadata to message
         # subject to change as metadata is accepted from drone
         (drone_id, lat, lon, alt, fov_h, fov_v, epoch_sec,) = unpack(">IdddddQ", metadata)
         update.drone_id = drone_id
-        pos = drone_pb2.LatLng()
-        pos.Latitude = lat
-        pos.Longitude = lon
-        update.position = pos
+        update.position.latitude = lat
+        update.position.longitude = lon
         update.altitude = alt
         update.fov_h = fov_h
         update.fov_v = fov_v
         update.message_time = dt.datetime.fromtimestamp(epoch_sec)
-        # update.drone_id = 1
-        # pos = drone_pb2.LatLng()
-        # pos.Latitude = 0
-        # pos.Longitude = 0
-        # update.position = pos
-        # update.altitude = 4
-        # update.fov_h = 65
-        # update.fov_v = 52
-        # update.message_time = Timestamp().GetCurrentTime()
 
         # add bounding boxes to message
         for xywh, label in zip(boxes.xywh, boxes.cls):
-            match label:
+            match self.model.names[int(label)]:
                 case "healthy":
-                    panel_status = "STATUS_CLEAN"
+                    panel_status = "STATUS_HEALTHY"
                 case "bird_drop" | "bird_feather" | "dust_partical" | "leaf" | "snow":
                     panel_status = "STATUS_DIRTY"                    
                 case "cracked":
@@ -83,36 +78,40 @@ class ImageServerProtocol:
                 case _:
                     panel_status = "STATUS_UNSPECIFIED"
 
-            new_bb = drone_pb2.boxes.add()
+            np_xywh = xywh.cpu().numpy()
+            new_bb = update.boxes.add()
             new_bb.status = drone_pb2.PanelStatus.Value(panel_status)
             # bounding box in x, y, width, height order
-            new_bb.x = xywh[0]
-            new_bb.y = xywh[1]
+            new_bb.x = int(math.floor(np_xywh[0]))
+            new_bb.y = int(math.floor(np_xywh[1]))
 
         # add image data + size to message
         update.height = HEIGHT
         update.width = WIDTH
-        update.image = image_bytes
+        with open(image_path, "rb") as png_bytes:
+            update.image = png_bytes.read()
 
         serialized_update = update.SerializeToString()
-        
-        requests.post(BACKEND_URL, data=serialized_update)
 
-        timestamp = dt.datetime.now().strftime("%Y%m%d%H%M%S_%f")
-        image_path = os.path.join(DEST_DIR, f"{timestamp}_pred.jpg")
-        results[0].show()
-        results[0].save(filename=image_path)
+        
+        print("posting update to backend")
+        r = requests.post(BACKEND_URL, data=serialized_update)
+        print(r.text)
+
 
     def receive_images(self):
         try:
             while True:
+                print("waiting to recieve image")
                 connection, client_addr = self.socket.accept()
 
                 try:
+                    metadata = connection.recv(52)
+
                     bs = connection.recv(8)
                     (length,) = unpack(">Q", bs)
+                    print(length)
                  
-                    metadata = connection.recv(52)
                     
                     data = b""
                     while len(data) < length:
@@ -123,6 +122,7 @@ class ImageServerProtocol:
 
                     connection.sendall(b"\00")
 
+                    print("recieved image + metadata")
                     self.process_image(data, metadata)
 
                 finally:
